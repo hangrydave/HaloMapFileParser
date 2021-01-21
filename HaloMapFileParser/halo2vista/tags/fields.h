89a918a86200a4b9c9947ff2a54fbc7eb7c12158
David Reidsma <david.reidsma@gmail.com>
2021-01-21 00:54:23 -0500

    macros are scary, i need a macro holiday

--- HaloMapFileParser/halo2vista/tags/fields.h
+++ HaloMapFileParser/halo2vista/tags/fields.h
@@ -1,18 +1,5 @@
 #pragma once
 
-#include <vector>
-#include <utility>
-#include <string>
-
-#define TAG_GROUP(name, size)								\															\
-	el_fuckery();											\
-	std::vector<std::pair<e_field, std::string>> fields
-
-void el_fuckery()
-{
-
-}
-
 enum e_field
 {
 	f_undefined,
@@ -26,9 +13,4 @@ enum e_field
 	f_tag_block,
 	f_tag_ref,
 	f_data_ref,
-};
-
-TAG_GROUP("eee", 1)
-{
-	{ f_bit, "e" }
 };
